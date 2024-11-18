5ae437b248323f37140b053292933039f6ebe959
Filippo29 <filodev06@gmail.com>
2024-11-18 11:57:27 +0100

    added game activity

--- app/src/main/java/com/snakehandgestures/MainActivity.kt
+++ app/src/main/java/com/snakehandgestures/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.snakehandgestures
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.snakehandgestures.ui.theme.SnakeHandGesturesTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +40,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier.fillMaxSize(), // make the Box take the entire screen
+        contentAlignment = Alignment.Center // center all elements inside the Box
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Welcome to the new generation Snake!"
+            )
+            Button(
+                onClick = {
+                    val intent = Intent(context, GameActivity::class.java)
+                    context.startActivity(intent)
+                },
+            ) {
+                Text(text = "Start Game")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
