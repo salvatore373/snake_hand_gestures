065b31ca0786ee92f1bf0101f2a0e128822334b8
Salvatore Rago <salvatorerago373@gmail.com>
2024-12-22 16:00:06 +0100

    Change landing activity

--- app/src/main/java/com/snakehandgestures/MainActivity.kt
+++ app/src/main/java/com/snakehandgestures/MainActivity.kt
@@ -5,18 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.snakehandgestures.ui.theme.SnakeHandGesturesTheme
@@ -50,16 +55,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier.padding(16.dp)
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.snake_head_green),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(128.dp),
+            )
             Text(
                 text = "Welcome to the new generation Snake!"
             )
-            Button(
-                onClick = {
-                    val intent = Intent(context, GameActivity::class.java)
-                    context.startActivity(intent)
-                },
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
             ) {
-                Text(text = "Start Game")
+                Button(
+                    onClick = {
+                        val intent = Intent(context, GameActivity::class.java)
+                        context.startActivity(intent)
+                    },
+                ) {
+                    Text(text = "Play")
+                }
+                OutlinedButton(
+                    onClick = {
+                        // TODO:
+                    },
+                ) {
+                    Text(text = "Leaderboard")
+                }
             }
         }
     }
